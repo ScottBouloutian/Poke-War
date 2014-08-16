7a719e934eb2348965c80376e5ab2d1c3f455c17
Scott Bouloutian <ScottBouloutian@gmail.com>
2014-08-16 11:52:15 -0400

    Added build info to the README. Beautified a couple files.

--- README.md
+++ README.md
@@ -2,3 +2,54 @@ Poke-War
 ========
 
 Headless Facebook poke war combatant.
+
+Getting Started
+-------
+
+* Clone this repository
+```
+git clone git@github.com:ScottBouloutian/Poke-War.git
+cd Poke-War
+npm install
+```
+
+* Create a Facebook app
+ * Goto [Facebook Dev Page](https://developers.facebook.com/)
+ * Apps > Create New App
+ * Choose a random title and category (e.g. Operation P.O.K.E. under Entertainment)
+ * Note the App ID and App Secret
+ * Update ```config.js``` with your App ID and App Secret
+
+
+* Start the Login server
+```
+node login
+```
+
+* Login to the Facebook app
+ * Open a web-browser and navigate to ```localhost:3000```
+ * Click the Login link and sign in to Facebook
+ * You should be redirected to a page saying 'Success'
+ * Copy the Access Token
+ * Close the web-browser
+ * Close the Login server (ctrl + c)
+
+
+* Create the secrets file
+```
+touch secrets.json
+```
+
+* Add the following configuration options to ```secrets.json``` :
+```
+{
+"email": "FACEBOOK_USER_NAME",
+"password": "FACEBOOK_PASSWORD",
+"accessToken": "YOUR_ACCESS_TOKEN"
+}
+```
+
+* If you've made it this far, do a celebratory dance and start the Poke-War!
+```
+node index
+```
--- routes/home.js
+++ routes/home.js
@@ -1,87 +1,88 @@
+var FB = require('fb'),
+  Step = require('step'),
 
-var FB              = require('fb'),
-    Step            = require('step'),
-
-    config          = require('../config');
+  config = require('../config');
 
 FB.options({
-    appId:          config.facebook.appId,
-    appSecret:      config.facebook.appSecret,
-    redirectUri:    config.facebook.redirectUri
+  appId: config.facebook.appId,
+  appSecret: config.facebook.appSecret,
+  redirectUri: config.facebook.redirectUri
 });
 
 exports.index = function(req, res) {
-    var accessToken = req.session.access_token;
-    if(!accessToken) {
-        res.render('index', {
-            title: 'Express',
-            loginUrl: FB.getLoginUrl({ scope: 'read_mailbox' })
-        });
-    } else {
-        console.log('Access token successfully retrieved');
-        res.render('success', {
-          accessToken: accessToken
-        });
-    }
+  var accessToken = req.session.access_token;
+  if (!accessToken) {
+    res.render('index', {
+      title: 'Express',
+      loginUrl: FB.getLoginUrl({
+        scope: 'read_mailbox'
+      })
+    });
+  } else {
+    console.log('Access token successfully retrieved');
+    res.render('success', {
+      accessToken: accessToken
+    });
+  }
 };
 
-exports.loginCallback = function (req, res, next) {
-    var code            = req.query.code;
+exports.loginCallback = function(req, res, next) {
+  var code = req.query.code;
 
-    if(req.query.error) {
-        // user might have disallowed the app
-        return res.send('login-error ' + req.query.error_description);
-    } else if(!code) {
+  if (req.query.error) {
+    // user might have disallowed the app
+    return res.send('login-error ' + req.query.error_description);
+  } else if (!code) {
+    return res.redirect('/');
+  }
+
+  Step(
+    function exchangeCodeForAccessToken() {
+      FB.napi('oauth/access_token', {
+        client_id: FB.options('appId'),
+        client_secret: FB.options('appSecret'),
+        redirect_uri: FB.options('redirectUri'),
+        code: code
+      }, this);
+    },
+    function extendAccessToken(err, result) {
+      if (err) throw (err);
+      FB.napi('oauth/access_token', {
+        client_id: FB.options('appId'),
+        client_secret: FB.options('appSecret'),
+        grant_type: 'fb_exchange_token',
+        fb_exchange_token: result.access_token
+      }, this);
+    },
+    function(err, result) {
+      if (err) return next(err);
+
+      req.session.access_token = result.access_token;
+      req.session.expires = result.expires || 0;
+
+      if (req.query.state) {
+        var parameters = JSON.parse(req.query.state);
+        parameters.access_token = req.session.access_token;
+
+        console.log(parameters);
+
+        FB.api('/me/' + config.facebook.appNamespace + ':eat', 'post', parameters, function(result) {
+          console.log(result);
+          if (!result || result.error) {
+            return res.send(500, result || 'error');
+            // return res.send(500, 'error');
+          }
+
+          return res.redirect('/');
+        });
+      } else {
         return res.redirect('/');
+      }
     }
-
-    Step(
-        function exchangeCodeForAccessToken() {
-            FB.napi('oauth/access_token', {
-                client_id:      FB.options('appId'),
-                client_secret:  FB.options('appSecret'),
-                redirect_uri:   FB.options('redirectUri'),
-                code:           code
-            }, this);
-        },
-        function extendAccessToken(err, result) {
-            if(err) throw(err);
-            FB.napi('oauth/access_token', {
-                client_id:          FB.options('appId'),
-                client_secret:      FB.options('appSecret'),
-                grant_type:         'fb_exchange_token',
-                fb_exchange_token:  result.access_token
-            }, this);
-        },
-        function (err, result) {
-            if(err) return next(err);
-
-            req.session.access_token    = result.access_token;
-            req.session.expires         = result.expires || 0;
-
-            if(req.query.state) {
-                var parameters              = JSON.parse(req.query.state);
-                parameters.access_token     = req.session.access_token;
-
-                console.log(parameters);
-
-                FB.api('/me/' + config.facebook.appNamespace +':eat', 'post', parameters , function (result) {
-                    console.log(result);
-                    if(!result || result.error) {
-                        return res.send(500, result || 'error');
-                        // return res.send(500, 'error');
-                    }
-
-                    return res.redirect('/');
-                });
-            } else {
-                return res.redirect('/');
-            }
-        }
-    );
+  );
 };
 
-exports.logout = function (req, res) {
-    req.session = null; // clear session
-    res.redirect('/');
+exports.logout = function(req, res) {
+  req.session = null; // clear session
+  res.redirect('/');
 };
